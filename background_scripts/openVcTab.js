d30b47a3cdff6012d1a846b5a41dd066c0f4b0d9
martindupeyron <martin.dupeyron@hotmail.fr>
2019-05-29 15:12:40 +0200

    ajout de l'url post à la structure dans le local storage

--- background_scripts/openVcTab.js
+++ background_scripts/openVcTab.js
@@ -2,6 +2,29 @@ browser.runtime.onMessage.addListener(notify);
 
 function notify(message) {
 	if(message.buttonVP != null){
+		sendViaXHR();
+		const struc = browser.storage.local.get("spStorage");
+		struc.then(function(item){
+			var spStorage = item;
+			spStorage.spStorage[0]['urlToPOST'] = message.buttonVP;
+			browser.storage.local.set(spStorage);
+		});
 		browser.tabs.create({url: "/TabVCs/exemple.html"});
 	}
+}
+
+function sendViaXHR() {
+  var js = {
+  	"titre" : "FIFA19"
+  };
+  let url = "https://example.com:5000/sendVP";
+  var xhrVP = new XMLHttpRequest();
+  xhrVP.open("POST", url, true);
+
+  xhrVP.onreadystatechange = function() {
+    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+      console.log("Request send");
+    }
+  }
+  xhrVP.send(js);
 }
